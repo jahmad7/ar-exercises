0cfa67270bd6b8a4c079cc5d62e4287739ea2004
Junaid <jahmad095@gmail.com>
2019-07-09 15:47:19 -0400

    complete 4

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -21,3 +21,25 @@ whistler = Store.new do |store|
     store.annual_revenue = 1900000
     store.mens_apparel = true
     store.womens_apparel = false
+end
+whistler.save
+
+yaletown = Store.new do |store|
+    store.name = "Yaletown"
+    store.annual_revenue = 430000
+    store.mens_apparel = true
+    store.womens_apparel = true
+end
+yaletown.save
+
+@mens_stores = Store.where(mens_apparel: true).order(created_at: :desc)
+@womens_stores = Store.where(womens_apparel: true).order(created_at: :desc)
+
+
+@mens_stores.select do |store|
+    puts "#{store.name} makes #{store.annual_revenue} each year"
+end 
+puts 
+@womens_stores.select do |store| store.annual_revenue < 1000000
+    puts "#{store.name} makes  #{store.annual_revenue}"
+end 
--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,5 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+@total_revenue = Store.sum(:annual_revenue)
+@average_revenue = Store.average(:annual_revenue)
