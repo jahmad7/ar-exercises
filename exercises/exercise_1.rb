e4641242e753677998e4b50e7a7ab56f700946d4
Junaid <jahmad095@gmail.com>
2019-07-09 14:19:35 -0400

    complete 1, 2, 3

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,29 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+
+burnaby = Store.new do |store|
+    store.name = "Burnaby"
+    store.annual_revenue = 300000
+    store.mens_apparel = true
+    store.womens_apparel = true
+end
+burnaby.save
+
+richmond = Store.new do |store|
+    store.name = "Richmond"
+    store.annual_revenue = 1260000
+    store.mens_apparel = false
+    store.womens_apparel = true
+end
+richmond.save
+
+gastown = Store.new do |store|
+    store.name = "Gastown"
+    store.annual_revenue = 190000
+    store.mens_apparel = true
+    store.womens_apparel = false
+end
+gastown.save
+
+puts "There are #{Store.count} Stores in record"
--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,17 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+surrey = Store.new do |store|
+    store.name = "Surrey"
+    store.annual_revenue = 224000
+    store.mens_apparel = false
+    store.womens_apparel = true
+end
+surrey.save
+
+
+whistler = Store.new do |store|
+    store.name = "Whistler"
+    store.annual_revenue = 1900000
+    store.mens_apparel = true
+    store.womens_apparel = false
